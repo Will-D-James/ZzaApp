ba3cd22f459112ca3f13ffa9babd2c49de7eaac3
William James <wdj1994@live.co.uk>
2018-07-22 12:42:47 +0100

    Add project files.

---
+++ ZzaServices/IZaaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace ZzaServices
+{
+    [ServiceContract]
+    public interface IZaaService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+
+        [OperationContract]
+        List<Customer> GetCustomers();
+
+        [OperationContract]
+        void SubmitOrder(Order order);
+
+
+    }
+}
---
+++ ZzaServices/ZzaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using Zza.Entities;
+
+namespace ZzaServices
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    class ZzaService : IZaaService, IDisposable
+    {
+        readonly ZzaDbContext _Context = new ZzaDbContext();
+
+        public void Dispose()
+        {
+            _Context.Dispose();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _Context.Customers.ToList();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _Context.Products.ToList();
+        }
+
+        [OperationBehavior(TransactionScopeRequired =true)]
+        public void SubmitOrder(Order order)
+        {
+            _Context.Orders.Add(order);
+            order.OrderItems.ForEach(x => _Context.OrderItems.Add(x));
+            _Context.SaveChanges();
+        }
+    }
+}
